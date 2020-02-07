385787bf01d1a60003c6f06b518428a07ad750a2
pra <prabhat933@gmail.com>
2020-02-07 21:33:49 +0530

    get data issue

--- weather/src/app/services/data.service.ts
+++ weather/src/app/services/data.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DataService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  getData() {
+    let _url: string = ` https://openweathermap.org/api`;
+    return this.http.get(_url);
+  }
 }
