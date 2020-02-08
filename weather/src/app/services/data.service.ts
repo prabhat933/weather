7c972188b2df968d2979cd5ea8a996ddccb6fdc3
pra <prabhat933@gmail.com>
2020-02-08 21:41:47 +0530

    Revert "pincode button doesnt show in listing component"
    This reverts commit 80d5c5cf75056d0dd9ab68568a2f1be1e599b198.

--- weather/src/app/services/data.service.ts
+++ weather/src/app/services/data.service.ts
@@ -6,8 +6,8 @@ import { HttpClient } from "@angular/common/http";
 })
 export class DataService {
   constructor(private http: HttpClient) {}
-  getData(pincode = "") {
-    let _url: string = `api.openweathermap.org/data/2.5/weather?zip=${pincode},us`;
+  getData() {
+    let _url: string = ` https://openweathermap.org/api`;
     return this.http.get(_url);
   }
 }
